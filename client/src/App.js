cd7a410d58ff0e3e4ac6381c79738357928e094f
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-05-11 14:19:55 +0800

    chore: App.js page and routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import NoPage from "./pages/NoPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup"
+import AdminHomePage from "./pages/AdminHomePage"
+import CustomerHomePage from "./pages/CustomerHomePage"
 
-
-function App() {
+const App  =() =>{
   return (
     <div className="App">
-      Placeholder
+      <BrowserRouter>
+        <div className="web_body">
+          <NavBar/>
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup/>} />
+              <Route path="/login" element={<Login/>} />
+              <Route path="/admin-homepage" element={<AdminHomePage/>} />
+              <Route path="/customer-homepage" element={<CustomerHomePage/>} />
+              <Route path="*" element={<NoPage/>} />
+          </Routes>
+        </div>
+    </BrowserRouter>
     </div>
   );
 }
