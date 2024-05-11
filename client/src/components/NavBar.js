8ffe07e3579bd2b1d275d19c720ed86ef8197202
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-05-11 22:28:43 +0800

    feat: working navigation bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import "./NavBar.css"
 
@@ -14,8 +14,8 @@ const NavBar = () => {
     {path : "/", icon: <LiaHomeSolid color={iconColor} size={iconSize}/>},
     {path : "/shopping", icon: <LiaShoppingBagSolid color={iconColor} size={iconSize}/>},
     {path : "/profile", icon: <PiUserCircleLight color={iconColor} size={iconSize} />},
-
   ]
+
 
   return (
     <>
@@ -27,7 +27,29 @@ const NavBar = () => {
             navBarItems.map((navBarItem,index) =>{
               return(
                 <Link key={index} to={navBarItem.path}>
-                  <li className='nav_bar-button'>
+                  <li 
+                    id={"nav_bar-button-" + index}
+                    className='nav_bar-button'
+                    onClick={(e) => {
+
+                      const buttons= document.querySelectorAll(`.nav_bar-button`)
+                      for (const key in buttons) {
+                        const button = buttons[key];
+                        if(button===null) break
+                        if(button.id === ("nav_bar-button-" + index)){
+                          button.children[0].className="nav_bar-selected"
+                        } else{
+                          try{
+                            button.children[0].className="nav_bar-selected no_display"
+                          } catch{
+                            console.log("Button null")
+                          }
+                        }
+                      }
+                      
+                    }}
+                  >
+                   <div id={"nav_bar-selected-" + index} className='nav_bar-selected'></div>
                    {navBarItem.icon}
                   </li>
                 </Link>
@@ -35,6 +57,10 @@ const NavBar = () => {
             })
           }
         </ul>
+        <div className='nav_bar-label'>
+          <div className='nav_bar-title'>F2T</div>
+          <div>@2024</div>
+        </div>
       </div>
     </>
   )
