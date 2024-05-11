96fc9a1b9285bf0be284dd8bdc82c6dee70c1fd5
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-05-11 14:20:49 +0800

    feat: added primary pages

---
+++ client/src/pages/CustomerHomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerHomePage = () => {
+  return (
+    <div className='customer_homepage page'>CustomerHomePage</div>
+  )
+}
+
+export default CustomerHomePage
