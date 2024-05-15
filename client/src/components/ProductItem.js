7e445ae41c52d751b0a1ee412854ed22a8a88736
KenAlwyn <ksvalenciano1@up.edu.ph>
2024-05-15 16:33:59 +0800

    chore: product qty added

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -10,6 +10,7 @@ const ProductItem = ({product}) => {
         <img src={product.image} alt={product.name}/>
         <div className='product_item-name'>{product.name}</div>
         <div className='product_item-price'>P {product.price}/kg</div>
+        <div className='product_item-qty'>Available Qty: {product.quantity} kgs</div>
         <button className='product_item-button'>
         <PiShoppingCartSimpleLight color={iconColor} size={'3ch'}/>
         Add To Cart
