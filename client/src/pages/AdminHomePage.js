71c842134f0e201d9b99d2156db81e4cda27e314
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-05-15 18:14:51 +0800

    test: sample admin homepage react

--- client/src/pages/AdminHomePage.js
+++ client/src/pages/AdminHomePage.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const AdminHomePage = () => {
   return (
-    <div className='admin_homepage page'>AdminHomePage</div>
+    <div className='admin_homepage page'>
+      <Header headerTitle={"Admin Homepage"}/>
+      <div>
+        <Card title="Food" imageUrl="https://th.bing.com/th/id/OIP.c8TLoLaBCRwtJV6wwwxJkwAAAA?rs=1&pid=ImgDetMain"/>
+        <Card  title="Clothes"  imageUrl="https://th.bing.com/th/id/OIP.c8TLoLaBCRwtJV6wwwxJkwAAAA?rs=1&pid=ImgDetMain"/>
+        <Card  title="Food"  imageUrl="https://th.bing.com/th/id/OIP.c8TLoLaBCRwtJV6wwwxJkwAAAA?rs=1&pid=ImgDetMain"/>
+      </div>
+    </div>
   )
+
 }
 
 export default AdminHomePage
+
+
+function Card(props) {
+  return <div className='card' style={{ width: "20%", height: "400px", backgroundColor: "var(--primary-green)" }}>
+    
+    <h2>{props.title}</h2>
+    
+    <div>
+      <img src={props.imageUrl}/>
+    </div>
+  </div>
+}
