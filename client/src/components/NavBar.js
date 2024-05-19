34a5eb9c0b3e07a1deea7bdb90a14cf483c7e2ca
KenAlwyn <ksvalenciano1@up.edu.ph>
2024-05-19 22:34:58 +0800

    feat: product listings navigation added

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,6 +8,7 @@ import { PiUserCircleLight } from "react-icons/pi";
 import { MdOutlineManageAccounts } from "react-icons/md";
 import { iconColor, iconSize } from '../constants/IconSize';
 import { IoReceiptOutline } from "react-icons/io5";
+import { MdFormatListBulleted } from "react-icons/md";
 
 
 const NavBar = () => {
@@ -16,7 +17,8 @@ const NavBar = () => {
     {path : "/shopping-page", icon: <LiaShoppingBagSolid color={iconColor} size={iconSize}/>},
     {path : "/profile", icon: <PiUserCircleLight color={iconColor} size={iconSize} />},
     {path : "/account-management", icon: <MdOutlineManageAccounts color={iconColor} size={iconSize} />},
-    {path : "/order-fulfillment", icon: <IoReceiptOutline color={iconColor} size={"3.5ch"} />}
+    {path : "/order-fulfillment", icon: <IoReceiptOutline color={iconColor} size={"3.5ch"} />},
+    {path : "/product-listings", icon: <MdFormatListBulleted color={iconColor} size={iconSize} />}
   ]
 
   const [selectedIndex, setselectedIndex] = useState(0)
