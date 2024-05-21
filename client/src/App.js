367ddc741a5a8817951af836d5f764ae57908ea8
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-05-21 14:37:56 +0800

    chore: frontend authentication with dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import NoPage from "./pages/NoPage";
 import Login from "./pages/Login";
@@ -8,33 +8,59 @@ import CustomerHomePage from "./pages/CustomerHomePage"
 import ShoppingPage from "./pages/ShoppingPage";
 import AccountManagement from "./pages/AccountManagement";
 import background from "./assets/images/background.png"
-import Messenger from "./pages/Messenger";
 import CartPage from "./pages/CartPage";
 import OrderFulfillment from "./pages/OrderFulfillment";
 import ProductListingsPage from "./pages/ProductListingsPage";
 import SalesReport from "./pages/SalesReport";
 import AddProductPage from "./pages/AddProductPage";
+import { UserType } from "./constants/UserType";
+import { iconColor, iconSize } from './constants/IconSize';
+
+import { LiaHomeSolid } from "react-icons/lia";
+import { LiaShoppingBagSolid } from "react-icons/lia";
+import { MdOutlineManageAccounts } from "react-icons/md";
+import { IoReceiptOutline } from "react-icons/io5";
+import { MdFormatListBulleted } from "react-icons/md";
+import { TbReport } from "react-icons/tb";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { PiShoppingCartSimpleLight } from "react-icons/pi";
+import { useState } from "react";
+import { LuLogOut } from "react-icons/lu";
+
+
 
 const App  =() =>{
+
+  const [userType,setUserType] = useState(UserType.GUEST)
+
+  const routeList = [
+    {path: "/", element: UserType.MERCHANT === userType? <AdminHomePage />: UserType.CUSTOMER === userType? <CustomerHomePage/>: <Login onSubmit={(type) => setUserType(type)}/>, visibility: ["all"],icon: <LiaHomeSolid color={iconColor} size={iconSize}/> },
+    {path: "/login", element: <Login onSubmit={(type) => setUserType(type)}/>, visibility: ["no_nav","all"]},
+    {path: "/signup", element: <Signup />, visibility: ["no_nav","all"]},
+    {path: "/admin-homepage", element: <AdminHomePage />, visibility: ["no_nav",UserType.MERCHANT]},
+    {path: "/customer-homepage", element: <CustomerHomePage />, visibility: ["no_nav",UserType.CUSTOMER]},
+    {path: "/shopping-page", element: <ShoppingPage />, visibility: [UserType.CUSTOMER],icon: <LiaShoppingBagSolid color={iconColor} size={iconSize}/>},
+    {path: "/cart", element: <CartPage />, visibility: [UserType.CUSTOMER],icon: <PiShoppingCartSimpleLight color={iconColor} size={iconSize}/>},
+    {path: "/account-management", element: <AccountManagement />, visibility: [UserType.MERCHANT],icon: <MdOutlineManageAccounts color={iconColor} size={iconSize}/>},
+    {path: "/order-fulfillment", element: <OrderFulfillment />, visibility: [UserType.MERCHANT],icon: <IoReceiptOutline color={iconColor} size={iconSize}/>},
+    {path: "/product-listings", element: <ProductListingsPage />, visibility: [UserType.MERCHANT],icon: <MdFormatListBulleted color={iconColor} size={iconSize}/>},
+    {path: "/sales-report", element: <SalesReport />, visibility: [UserType.MERCHANT],icon: <TbReport color={iconColor} size={iconSize}/>},
+    {path: "/add-product", element: <AddProductPage />, visibility: [UserType.MERCHANT],icon: <IoIosAddCircleOutline color={iconColor} size={iconSize}/>},
+    {path: "/logout", element: <Navigate to="/"/>, visibility: ["all"], icon: <LuLogOut color={iconColor} size={"3.5ch"}/>},
+    {path: "/*", element: <NoPage />, visibility: ["none"]},
+  ]
+
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar/>
+        {userType === UserType.GUEST? null : <NavBar onLogout={()=>setUserType(UserType.GUEST)} navBarItems = {routeList.filter(route=> route.visibility[0] !== "no_nav" && (route.visibility.includes("all") || route.visibility.includes(userType)) )}/>}
         <div className="web_body">
           <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/signup" element={<Signup/>} />
-              <Route path="/login" element={<Login/>} />
-              <Route path="/admin-homepage" element={<AdminHomePage/>} />
-              <Route path="/customer-homepage" element={<CustomerHomePage/>} />
-              <Route path="/shopping-page" element={<ShoppingPage/>} />
-              <Route path="/cart" element={<CartPage/>} />
-              <Route path="/account-management" element={<AccountManagement/>} />
-              <Route path="/order-fulfillment" element={<OrderFulfillment/>} />
-              <Route path="/product-listings" element={<ProductListingsPage/>} />
-              <Route path="/sales-report" element={<SalesReport/>} />
-              <Route path="/add-product" element={<AddProductPage/>} />
-              <Route path="*" element={<Messenger/>} />
+              {
+                routeList.map((route,index)=>
+                  <Route key={index} path={route.path} element={route.visibility.includes("all")? route.element: route.visibility.includes(userType)? route.element: <NoPage/>} />
+                )
+              }
           </Routes>
           <img className="page_background_decor" src={background}/>
         </div>
