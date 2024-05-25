af22df89da08560e4be55f24ab993a1967813c63
JomarMonrealUPLB <jpmonreal@up.edu.ph>
2024-05-26 04:36:13 +0800

    feat: Shopping Cart Integration

--- client/src/components/CartTable.js
+++ client/src/components/CartTable.js
@@ -4,14 +4,46 @@ import './CartTable.css'
 const getTotal = (cartItems) =>{
   let total = 0
   cartItems.map(cartItem => {
-    total += (cartItem.quantity * cartItem.price)
+    total += (cartItem.quantity * cartItem.product.price)
   })
   return total
 }
 
 const CartTable = ({cartData}) => {
-  const [total, setTotal] = useState(getTotal(cartData))
-  const [cartItems, setCartItems] = useState(cartData)
+  const [total, setTotal] = useState(0)
+  const [orders, setOrders] = useState([])
+  const [cartItems, setCartItems] = useState([])
+  
+
+  useEffect(() => {
+    fetch('http://localhost:3000/orders/status/0')
+          .then(response => response.json())
+          .then(body => {
+              setOrders(body)
+          })
+    return () => {};
+  }, []);
+
+  useEffect(() => {
+    const fetchProducts = async ()=>{
+      const tempCart = []
+      const promises = orders.map(order => {
+        const promise = fetch(
+          `http://localhost:3000/products/${order.productID}`
+        ).then(response => response.json()).then(body=> {tempCart.push({...order, product: body})})
+        return promise
+      });
+      
+      await Promise.all(promises)
+      setCartItems(tempCart)
+      console.log(tempCart)
+    }
+
+    fetchProducts()
+    return () => {
+      
+    };
+  }, [orders]);
 
   useEffect(() => {
     setTotal(getTotal(cartItems))
@@ -33,43 +65,70 @@ const CartTable = ({cartData}) => {
           
           {
             cartItems.map((cartItem, index) => (
-              <tr className='cart_table-row' key={cartItem.name}>
+              <tr className='cart_table-row' key={cartItem.product.name}>
                 <td className='cart_table-row-img'>
-                  <img src={cartItem.image} alt={cartItem.name}/>
+                  <img src={cartItem.product.image} alt={cartItem.product.name}/>
                 </td>
                 <td className='cart_table-row-name'>
-                  {cartItem.name}<br/>
+                  {cartItem.product.name}<br/>
                   <button className='cart_table-row-button' onClick={()=>{
                     setCartItems(
                       [...cartItems.slice(0,index),
                       ...cartItems.slice(index+1, cartItems.length)]
                     )
+                    fetch(`http://localhost:3000/orders/${cartItem._id}`,
+                        {
+                          method: 'DELETE',
+                          headers: {
+                            'Content-Type': 'application/json'
+                          },
+                        })
+                        .then(response => {
+                          response.text()
+                        })
                   }}>Remove</button>
                 </td>
 
-                <td className='cart_table-row-price'>P {cartItem.price}</td>
+                <td className='cart_table-row-price'>P {cartItem.product.price}</td>
                 <td className='cart_table-row-qty'>
                   <input type='number' value={cartItem.quantity} 
                     onChange={(e)=>{
-                      const temp = {...cartItems[index], quantity: e.target.value}
-  
-                      if(e.target.value==="0") {
-                        setCartItems(
-                          [...cartItems.slice(0,index),
-                        
-                          ...cartItems.slice(index+1, cartItems.length)]
-                        )
+                      let currentValue = parseInt(e.target.value)
+                      
+                      if(currentValue > cartItem.product.quantity){
+                        currentValue = cartItem.product.quantity
+                        e.target.value = currentValue
+                        return alert(`Order quantity musn't exceed product quantity (max: ${cartItem.product.quantity})`)
                       }
-                      else{
-                        setCartItems(
-                          [...cartItems.slice(0,index),
-                          temp,
-                          ...cartItems.slice(index+1, cartItems.length)]
-                        )
+
+                      if(currentValue < 0){
+                        return alert(`Order quantity must be greater than 0.`)
                       }
+
+                      const temp = {...cartItems[index], quantity: currentValue}
+
+                      fetch(`http://localhost:3000/orders/${cartItem._id}`,
+                      {
+                        credentials: 'include',
+                        method: 'PATCH',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({quantity: currentValue})
+                      })
+                      .then(response => {
+                        response.text()
+                      })
+                      
+                      setCartItems(
+                        [...cartItems.slice(0,index),
+                        temp,
+                        ...cartItems.slice(index+1, cartItems.length)]
+                      )
+
                   }}/>
                 </td>
-                <td className='cart_table-row-total'>P {cartItem.quantity * cartItem.price}</td>
+                <td className='cart_table-row-total'>P {cartItem.quantity * cartItem.product.price}</td>
               </tr>
             ))
           }
@@ -88,7 +147,27 @@ const CartTable = ({cartData}) => {
             <td/>
             <td/>
             <td/>
-            <td><button className='footer-button-checkout' onClick={()=>{}}>CHECK OUT</button></td>
+            <td><button className='footer-button-checkout' onClick={async ()=>{
+
+              const orderPromises = cartItems.map(async cartItem=>{
+                return fetch(`http://localhost:3000/orders/${cartItem._id}`,
+                {
+                  credentials: 'include',
+                  method: 'PATCH',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({status: 1})
+                })
+                .then(response => {
+                  response.text()
+                })
+              })
+              await Promise.all(orderPromises)
+
+              alert("Checkout complete!")
+              window.location.href = "http://localhost:3001/shopping-page"
+            }}>CHECK OUT</button></td>
           </tr>
         </tfoot>
     </table>
