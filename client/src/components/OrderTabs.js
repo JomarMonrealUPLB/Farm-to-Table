7d531715e44ebe12fa5b46f60facaca2f8fc6358
KenAlwyn <ksvalenciano1@up.edu.ph>
2024-05-27 21:31:38 +0800

    feat: fetch and filter orders added

--- client/src/components/OrderTabs.js
+++ client/src/components/OrderTabs.js
@@ -1,22 +1,71 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import OrderItem from '../components/OrderItem'
 import './OrderTabs.css'
+import Header from './Header'
 
-const OrderTabs = ({orders}) => {
+const getProductName = async (id) => {
+    const result = await fetch(
+        `http://localhost:3000/products/${id}`
+    ).then(response => response.json())
+    return result.name
+}
+
+const getProductImage = async (id) => {
+    const result = await fetch(
+        `http://localhost:3000/products/${id}`
+    ).then(response => response.json())
+    return result.image
+}
+
+const OrderTabs = ({email}) => {
+    const [orders, setOrders] = useState([])
+    const [filteredOrders, setfilteredOrders] = useState([])
     const [activeTab, setActiveTab] = useState(1)
 
+    useEffect(() => {
+        fetch(`http://localhost:3000/orders-by-email?email=${email}`)
+        .then(response => response.json())
+        .then(data => setOrders(data))
+    }, [email])
+
+    useEffect(() => {
+        const fetchAndSerializeData = async () => {
+            const serializedData = await Promise.all(
+                orders.map(async order => {
+                    if(order.status !== 0){
+                        const productImage = await getProductImage(order.productID)
+                        const productName = await getProductName(order.productID)
+                        return{
+                            id: order._id,
+                            image: productImage,
+                            name: productName,
+                            date: order.date,
+                            quantity: order.quantity,
+                            status: order.status
+                        }
+                    }
+                    return null
+                })
+            )
+            const filteredData = serializedData.filter(element => element !== null)
+            setfilteredOrders(filteredData)
+        }
+        fetchAndSerializeData()
+    }, [orders])
+
     const renderOrders = (status) => {
-        return orders
+        return filteredOrders
             .filter(order => order.status === status)
             .map(order => <OrderItem key={order.id} order={order} />);
     }
 
     return (
     <div className='order-tabs'>
-                <div className="tabs">
-                <button className={activeTab === 1 ? 'active' : ''} onClick={() => setActiveTab(1)}>Pending</button>
-                <button className={activeTab === 2 ? 'active' : ''} onClick={() => setActiveTab(2)}>Fulfilled</button>
-                <button className={activeTab === 3 ? 'active' : ''} onClick={() => setActiveTab(3)}>Cancelled</button>
+        <Header headerTitle={'Orders'}/>
+        <div className="tabs">
+            <button className={activeTab === 1 ? 'active' : ''} onClick={() => setActiveTab(1)}>Pending</button>
+            <button className={activeTab === 2 ? 'active' : ''} onClick={() => setActiveTab(2)}>Fulfilled</button>
+            <button className={activeTab === 3 ? 'active' : ''} onClick={() => setActiveTab(3)}>Cancelled</button>
         </div>
         <div className="tab-content">
             <table className='order-table' cellSpacing={0}>
